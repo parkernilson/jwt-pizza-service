58cc4dd7307621de37644322d1402fead1040131
Parker Nilson <parker.todd.nilson@gmail.com>
2024-09-30 13:05:50 -0600

    #d3: add test files and coverage config

---
+++ tests/database/database.test.js
@@ -0,0 +1,24 @@
+// Should get a menu
+// Should get a menu item
+// Should add a user
+// Should get a user
+// Should update a user
+// Should login a user
+// Should get whether a user is logged in
+// Should logout a user
+// Should get orders
+// Should add a diner order
+// Should create a franchise
+// Should delete a franchise
+// Should get all franchises
+// Should get a user's franchises
+// Should get a franchise
+// Should create a store
+// Should delete a store
+// Should get offset
+// Should get a token signature
+// Should execute a query
+// Should get ID
+// Should get a connection
+// Should initialize the database
+// Should check database exists
