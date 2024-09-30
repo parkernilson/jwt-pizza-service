62eb3d4133092fbad44b63c72739994b0c2e3286
Parker Nilson <parker.todd.nilson@gmail.com>
2024-09-30 13:14:02 -0600

    #d3: enable jest types

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,4 +6,5 @@ export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
   {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
+  { env: { jest: true } }
 ];
