70174550177342fe20b75429413648a77bf16932
Parker Nilson <parker.todd.nilson@gmail.com>
2024-11-30 17:28:08 -0700

    #d9: Add dbLogger to query method

--- src/database/database.js
+++ src/database/database.js
@@ -4,6 +4,8 @@ const config = require('../config.js');
 const { StatusCodeError } = require('../endpointHelper.js');
 const { Role } = require('../model/model.js');
 const dbModel = require('./dbModel.js');
+const logger = require('../logging/logger.js');
+
 class DB {
   constructor() {
     this.initialized = this.initializeDatabase();
@@ -285,6 +287,7 @@ class DB {
   }
 
   async query(connection, sql, params) {
+    logger.dbLogger(sql)
     const [results] = await connection.execute(sql, params);
     return results;
   }
